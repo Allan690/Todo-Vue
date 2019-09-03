4ed07f421c679bf788ac1ccff47a7f6b39a8f074
Allan Mogusu <allannelly690@gmail.com>
2019-09-03 18:26:23 +0300

    Todo tutorial 1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo-app-component></todo-app-component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todoAppComponent from './components/TodoComponent';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    todoAppComponent
   }
 }
 </script>
---
+++ src/components/TodoComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="todoItem">
+        <input v-model="todoItem">
+        <button v-on:click="addTodo">Add Todo</button>
+        <ul v-for="(todo, index) in todoArray" v-bind:key="index">
+            <li v-on:click="removeTodo">
+                {{ todo }}
+            </li>
+            <button v-if="todo">Display Conditionally</button>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'todoAppComponent',
+    data() {
+        return { todoItem: '', todoArray: [] }
+    },
+    methods: {
+        addTodo() {
+            this.todoArray.push(this.todoItem);
+        },
+        removeTodo() {
+            this.todoArray.pop(this.todoItem)
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+        console.log('mounted lifecycle hook called...')
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
